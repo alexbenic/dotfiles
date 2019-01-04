675c7a4056622ceddc36a227e4063f163c5952c5
Aleksandar Benic <aleksandar.benic@protonmail.com>
2019-01-04 01:44:06 +0100

    feat(polybar): adapt polybar for multiple monitors

--- polybar/.config/polybar/lunch.sh
+++ polybar/.config/polybar/lunch.sh
@@ -6,4 +6,13 @@ while pgrep -u $UID -x polybar >/dev/null; do
   sleep 1
 done
 
-polybar top &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    echo $m
+    MONITOR=$m polybar --reload top &
+  done
+else
+  polybar --reload top &
+fi
+
+# polybar top &
